d334c80edfd33b44e3333a8df887353ddf00d72b
Caio Andrade <caionarutolegal@gmail.com>
2021-05-31 17:44:42 -0300

    Alterando o projeto sozinho e jogando no git

--- src/application/main.java
+++ src/application/main.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello World!");
-
+		int a = sc.nextInt();
+		System.out.println("a vale -> " + a);
 	}
 
 }
